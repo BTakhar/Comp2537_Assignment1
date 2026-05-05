dbbf683b845a531cb293d7a4365181e824c24966
Btakhar <btakhar7@my.bcit.ca>
2026-05-05 02:03:33 -0700

    final final final

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-process.on('uncaughtException', (err) => {
-  console.error('🔥 Uncaught Exception:', err);
-});
-
-process.on('unhandledRejection', (err) => {
-  console.error('🔥 Unhandled Rejection:', err);
-});
-
 require('dotenv').config();
 
 const express = require('express');
